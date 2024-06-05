b12a264bebecc2085cb8e939f19910a3ae4bcc73
jacobperry0909 <person132125@outlook.com>
2024-06-05 18:53:24 -0400

    main changes

--- src/index.js
+++ src/index.js
@@ -10,6 +10,19 @@ class Logger {
 		this.useColors = useColors || true;
 		this.useConsole = useConsole || true;
 		this.customCodes = {};
+		this.useShortOnly = true;
+	}
+
+	createTimeStamp() {
+		const now = new Date();
+		const month = String(now.getMonth() + 1).padStart(2, '0');
+		const day = String(now.getDate()).padStart(2, '0');
+		const year = now.getFullYear();
+		const hour = String(now.getHours()).padStart(2, '0');
+		const minute = String(now.getMinutes()).padStart(2, '0');
+		const second = String(now.getSeconds()).padStart(2, '0');
+
+		return `${month}/${day}/${year} ${hour}:${minute}:${second}`;
 	}
 
 	log(msg, color) {
@@ -20,9 +33,41 @@ class Logger {
 	}
 
 	logCode({ code, msg }) {
-		let consoleOutput = '';
+		if (!code)
+			throw new Error(
+				'Must provide either a custom code, or a http code found on https://developer.mozilla.org/en-US/docs/Web/HTTP/Status'
+			);
+		if (typeof code !== 'string') {
+			code = `${code}`;
+		}
 		let finalOutput = '';
-		if (code && msg) {
+		let color;
+		let foundCode;
+		for (const key in this.customCodes) {
+			if (key === code) {
+				foundCode = this.customCodes[key];
+				color = this.customCodes[key].conf_Color;
+			}
+		}
+		if (!foundCode) {
+			for (const key in codes) {
+				if (key === code) {
+					foundCode = codes[key];
+					color = codes[`${key.charAt(0)}xx`].conf_Color;
+				}
+			}
+		}
+
+		if (msg) {
+			console.log(colors[color](`[${code}] - ${foundCode.short} | ${msg}`));
+		} else {
+			console.log(
+				colors[color](
+					`[${code}] - [${this.createTimeStamp()}] - ${foundCode.short} ${
+						this.useShortOnly ? '' : ` | ${foundCode.large}`
+					}`
+				)
+			);
 		}
 	}
 	addCustomCode(code, name, msg, color) {
@@ -33,12 +78,17 @@ class Logger {
 				'Either disable colors, or add a color to your custom code.'
 			);
 
-		this.customCodes[code] = { short: name, long: msg, color: color };
+		this.customCodes[code] = { short: name, large: msg, conf_Color: color };
 	}
 }
 
 const logger = new Logger();
 
-logger.log('Testing', 'red');
+// testing ↴
+
+logger.addCustomCode('701', 'test', 'testing', 'blue');
+logger.logCode({ code: '701', msg: 'Testing' });
+logger.logCode({ code: '200', msg: 'Success' });
+logger.logCode({ code: '400' });
 
 module.exports = logger;
